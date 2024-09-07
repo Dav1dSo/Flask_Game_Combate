c88539ed9666587dac5f58fe4b2e54eeef406c7c
David Soares <davidsoares.dev@gmail.com>
2024-09-06 22:25:32 -0300

    feat: Implement game logic and basic gameplay
    Adds core game loop, character interaction, and attack mechanics. Players can now choose between normal and special attacks, with the enemy retaliating accordingly. The game concludes when either the hero or the enemy is defeated.

--- main.py
+++ main.py
@@ -14,7 +14,22 @@ class Personagem:
         return f"{self.__nivel}"
 
     def exibe_detalhes(self):
-        return f"Nome: {self.__nome} \n Vida: {self.__vida} \n Nível: {self.__nivel}"
+        return f"\nNome: {self.__nome} \n Vida: {self.__vida} \n Nível: {self.__nivel} \n"
+    
+    def recebeAtaque(self, dano):
+        self.__vida -= dano
+        if self.__vida < 0:
+            self.__vida = 0 
+    
+    def ataque(self, alvo):
+        dano = self.__nivel * 2
+        alvo.recebeAtaque(dano)
+        print(f"\n {self.__nome} atacou {alvo.get_nome()} e causou {dano} de dano!")
+        
+    def ataqueEspecial(self, alvo):
+        dano = self.__nivel * 5
+        alvo.recebeAtaque(dano) 
+        print(f"{self.get_nome()} soltou o especial em {alvo.get_nome()} e causou {dano} de dano!")
 
 
 class Heroi(Personagem):
@@ -26,8 +41,7 @@ class Heroi(Personagem):
         return f"{self.__habilidade}"
 
     def exibe_detalhes(self):
-        return f"{super().exibe_detalhes()} \n Habilidade: {self.__habilidade}"
-
+        return f"{super().exibe_detalhes()} Habilidade: {self.__habilidade}"
 
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo) -> None:
@@ -38,26 +52,36 @@ class Inimigo(Personagem):
         return f"{self.__tipo}"
 
     def exibe_detalhes(self):
-        return f"{super().exibe_detalhes()} \nTipo: {self.__tipo}"
+        print(f"{super().exibe_detalhes()} Tipo: {self.__tipo}")
 
 
 class Jogo:
+
     def __init__(self) -> None:
-        self.heroi = Heroi("Galonilda", 1200, 43, "Atirar ovo")
-        self.inimigo = Inimigo("Bob_Esponjo", 4500, 100, "potente")
-
+        self.heroi = Heroi("Galonilda", 100, 1,  "Atirar ovo")
+        self.inimigo = Inimigo("Bob_Esponjo", 100, 10, "potente")
+    print('teste')
     def iniciar_jogo(self):
-        print(
-            "************************************* \n Jogo iniciado! \n************************************* \n"
-        )
+        print("********* Jogo iniciado! ********* \n")
 
-        print(self.heroi.exibe_detalhes())
-        print(self.inimigo.exibe_detalhes())
 
         while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
-            print(" \n Escolha uma opção:")
-            escolha = input(" 1 - Ataque normal\n 2 - Ataque Potente ")
-
+            print(self.heroi.exibe_detalhes())
+            print(self.inimigo.exibe_detalhes())
+            print("\n Escolha uma opção:")
+            escolha = input(str(" 1 - Ataque normal\n 2 - Ataque Potente\n"))
+            
+            if escolha == "1":
+                self.heroi.ataque(self.inimigo)
+            elif escolha == "2":
+                self.heroi.ataqueEspecial(self.inimigo)
+            else:
+                print("Escolha inválida! Escolha novamente! \n")
+                
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.ataque(self.heroi)
+                
+        print(f"\nParabéns! Você venceu o {self.inimigo.get_nome()}!")
 
 play = Jogo()
 play.iniciar_jogo()
